6d7fcbae4116da63afaa71d4714c1bd7eb191142
Alexey Snytnikov <snytav@gmail.com>
2024-01-29 23:15:51 +0200

    reduced nt to 5

---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from convection_basic import linear_convection_solve
+import numpy
+import matplotlib.pyplot as plt
+from matplotlib import pyplot, cm
+
+Lx = 2
+
+
+
+nx = 41  # try changing this number from 41 to 81 and Run All ... what happens?
+dx = 2 / (nx-1)
+nt = 5    #nt is the number of timesteps we want to calculate
+dt = .025  #dt is the amount of time each timestep covers (delta t)
+c = 1      #assume wavespeed of c = 1
+Lt = dt*nt
+u = numpy.zeros(nx)      #numpy function ones()
+#u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+x = np.linspace(0,1,int(nx/2))
+u[:int(nx/2)] = np.sin(np.pi*x)
+
+#from surface import draw_surf
+
+u,u2D = linear_convection_solve(u,c,dx,dt,Lx, nx, Lt, nt)
+
+#draw_surf(Lx,nx,Lt,nt,u2D) #(u,c,dx,dt,Lx, nx, Lt, nt)
+
+qq = 0
