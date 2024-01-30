2980041073c94f5f5d8def8d25f8115190f86891
Alexey Snytnikov <snytav@gmail.com>
2024-01-31 00:05:36 +0200

    initial condition based ob sine

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ dt = .025  #dt is the amount of time each timestep covers (delta t)
 c = 1      #assume wavespeed of c = 1
 Lt = dt*nt
 u = numpy.zeros(nx)      #numpy function ones()
-#u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+# u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
 x = np.linspace(0,1,int(nx/2))
 u[:int(nx/2)] = np.sin(np.pi*x)
 
-#from surface import draw_surf
+from surface import draw_surf
 
 u,u2D = linear_convection_solve(u,c,dx,dt,Lx, nx, Lt, nt)
 
-#draw_surf(Lx,nx,Lt,nt,u2D) #(u,c,dx,dt,Lx, nx, Lt, nt)
+draw_surf(Lx,nx,Lt,nt,u2D) #(u,c,dx,dt,Lx, nx, Lt, nt)
 
 qq = 0
