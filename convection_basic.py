e564bc3f87f8d03b4df8f8c74d1facf76674c2ba
Alexey Snytnikov <snytav@gmail.com>
2024-01-31 23:16:27 +0200

    most params hidden

--- convection_basic.py
+++ convection_basic.py
@@ -18,7 +18,20 @@ c = 1      #assume wavespeed of c = 1
 
 # u = numpy.ones(nx)  #numpy function ones()
 # u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
-def linear_convection_solve(u,c,dx,dt,Lx, nx, Lt, nt):
+def linear_convection_solve(c,Lx, nx, Lt, nt):
+    nx = 41  # try changing this number from 41 to 81 and Run All ... what happens?
+    dx = 2 / (nx - 1)
+    nt = 5  # nt is the number of timesteps we want to calculate
+    dt = .025  # dt is the amount of time each timestep covers (delta t)
+    c = 1  # assume wavespeed of c = 1
+    Lt = dt * nt
+    u = numpy.zeros(nx)  # numpy function ones()
+    # u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+    x = numpy.linspace(0, 1, int(nx / 2))
+    u[:int(nx / 2)] = numpy.sin(numpy.pi * x)
+
+    from surface import draw_surf
+
     nx = u.shape[0]
     u2D = numpy.zeros((nt,nx))
     pyplot.plot(numpy.linspace(0, 2, nx), u);
--- main.py
+++ main.py
@@ -10,20 +10,20 @@ Lx = 2
 
 
 nx = 41  # try changing this number from 41 to 81 and Run All ... what happens?
-dx = 2 / (nx-1)
-nt = 5    #nt is the number of timesteps we want to calculate
-dt = .025  #dt is the amount of time each timestep covers (delta t)
-c = 1      #assume wavespeed of c = 1
-Lt = dt*nt
-u = numpy.zeros(nx)      #numpy function ones()
-# u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
-x = np.linspace(0,1,int(nx/2))
-u[:int(nx/2)] = np.sin(np.pi*x)
+# dx = 2 / (nx-1)
+# nt = 5    #nt is the number of timesteps we want to calculate
+# dt = .025  #dt is the amount of time each timestep covers (delta t)
+# c = 1      #assume wavespeed of c = 1
+Lt = 0.025*5
+# u = numpy.zeros(nx)      #numpy function ones()
+# # u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+# x = np.linspace(0,1,int(nx/2))
+# u[:int(nx/2)] = np.sin(np.pi*x)
+#
+# from surface import draw_surf
 
-from surface import draw_surf
+u,u2D = linear_convection_solve(1.0,Lx, nx, Lt, 5)
 
-u,u2D = linear_convection_solve(u,c,dx,dt,Lx, nx, Lt, nt)
-
-draw_surf(Lx,nx,Lt,nt,u2D) #(u,c,dx,dt,Lx, nx, Lt, nt)
+ #(u,c,dx,dt,Lx, nx, Lt, nt)
 
 qq = 0
